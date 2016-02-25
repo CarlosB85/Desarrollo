a306ad9554a23fe861a0d4dc03b7a4ea11dafb59
CarlosB85 <carlos.brugnoni@gmail.com>
2016-02-24 20:39:21 -0500

    Ki-ZIP v 1.0

---
+++ Ki-ZIP v1.0/Ki-ZIP v1.0/Encoder.cs
@@ -0,0 +1,170 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Ki_ZIP_v1._0
+{
+    public class Encoder
+    {
+
+        private KinectSensor sensor;
+        private byte[] colorPixels;        
+
+        public Encoder()
+        {
+        }
+
+        public void iniciarVariables()
+        {
+
+            foreach (var potentialSensor in KinectSensor.KinectSensors)
+            {
+                Console.WriteLine("Eligiendo Sensor");
+                Console.CursorTop = Console.CursorTop -= 1;
+                Console.CursorLeft = 50;
+                if (potentialSensor.Status == KinectStatus.Connected)
+                {
+                    this.sensor = potentialSensor;
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine("OK");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("NO");
+                    Console.ResetColor();
+                }
+
+                Console.WriteLine("Habilitando Flujos");
+
+                if (this.sensor != null)
+                {
+                    Console.WriteLine("Flujo de Color");
+                    Console.CursorTop = Console.CursorTop -= 1;
+                    Console.CursorLeft = 50;
+                    this.sensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine(this.sensor.ColorStream.IsEnabled ? "OK" : "NO");
+                    Console.ResetColor();
+                    Console.WriteLine("Flujo de Profundidad");
+                    Console.CursorTop = Console.CursorTop -= 1;
+                    Console.CursorLeft = 50;
+                    this.sensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine(this.sensor.DepthStream.IsEnabled ? "OK" : "NO");
+                    Console.ResetColor();
+                    Console.WriteLine("Flujo de Skeleton");
+                    Console.CursorTop = Console.CursorTop -= 1;
+                    Console.CursorLeft = 50;
+                    this.sensor.SkeletonStream.Enable();
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine(this.sensor.SkeletonStream.IsEnabled ? "OK" : "NO");
+                    Console.ResetColor();
+                    Console.WriteLine("Flujo de Infrarojo");
+                    Console.CursorTop = Console.CursorTop -= 1;
+                    Console.CursorLeft = 50;
+                    this.sensor.ColorStream.Enable(ColorImageFormat.InfraredResolution640x480Fps30);
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine(this.sensor.ColorStream.IsEnabled ? "OK" : "NO");
+                    Console.ResetColor();
+
+                }
+            }
+        }
+
+        public void estado()
+        {
+            Console.WriteLine("Estado Sensor");
+            Console.CursorTop = Console.CursorTop -= 1;
+            Console.CursorLeft = 50;
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine(this.sensor.Status);
+            Console.ResetColor();
+            Console.WriteLine("Flujo de Color");
+            Console.CursorTop = Console.CursorTop -= 1;
+            Console.CursorLeft = 50;
+            escribirRespuesta(this.sensor.ColorStream.IsEnabled);
+            Console.WriteLine("Flujo de Profundidad");
+            Console.CursorTop = Console.CursorTop -= 1;
+            Console.CursorLeft = 50;
+            escribirRespuesta(this.sensor.DepthStream.IsEnabled);
+            Console.WriteLine("Flujo de Skeleton");
+            Console.CursorTop = Console.CursorTop -= 1;
+            Console.CursorLeft = 50;
+            escribirRespuesta(this.sensor.SkeletonStream.IsEnabled);
+            Console.WriteLine("Flujo de Infrarojo");
+            Console.CursorTop = Console.CursorTop -= 1;
+            Console.CursorLeft = 50;
+            escribirRespuesta(this.sensor.ColorStream.IsEnabled);
+            Console.WriteLine("Sensor transmitiendo");
+            Console.CursorTop = Console.CursorTop -= 1;
+            Console.CursorLeft = 50;
+            escribirRespuesta(this.sensor.IsRunning);
+        }
+
+        public void arrancar()
+        {
+            if (this.sensor != null)
+            {
+                Console.WriteLine("Iniciando transmision");
+                Console.CursorTop = Console.CursorTop -= 1;
+                Console.CursorLeft = 50;
+                this.sensor.Start();
+                escribirRespuesta(sensor.IsRunning);
+                this.colorPixels = new byte[this.sensor.ColorStream.FramePixelDataLength];
+                Console.WriteLine("Adjuntando metodo de captura");                
+                this.sensor.ColorFrameReady += this.SensorColorFrameReady;
+                Console.CursorTop = Console.CursorTop -= 1;
+                Console.CursorLeft = 50;
+                escribirRespuesta(true);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Sensor no conectado");
+                Console.ResetColor();
+            }
+        }
+
+        public void salir()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Deteniendo flujo");
+            this.sensor.Stop();
+        }
+
+        private void escribirRespuesta(bool respuesta)
+        {
+            if (respuesta)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("OK");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("NO");
+                Console.ResetColor();
+            }
+        }
+
+        #region metodos eventos
+        private void SensorColorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            using (ColorImageFrame colorFrame = e.OpenColorImageFrame())
+            {
+                if (colorFrame != null)
+                {
+                    // Write the pixel data into our bitmap
+                    this.colorBitmap
+                        //WritePixels(new Int32Rect(0, 0, this.colorBitmap.PixelWidth, this.colorBitmap.PixelHeight),this.colorPixels, this.colorBitmap.PixelWidth * sizeof(int),0);
+                }
+            }
+        }
+        #endregion
+    }
+}
---
+++ Ki-ZIP v1.0/Ki-ZIP v1.0/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Ki_ZIP_v1._0
+{
+    class Program
+    {
+        private Encoder _encoder;
+
+        static void Main(string[] args)
+        {
+            Program pp = new Program();
+            pp.iniciar();
+        }
+
+        public void iniciar()
+        {
+            _encoder = new Encoder();
+            _encoder.iniciarVariables();
+
+            bool noSalir = true;
+            while (noSalir)
+            {
+                String mensaje = Console.ReadLine();
+                switch (mensaje)
+                {
+                    case "iniciar":
+                        _encoder.arrancar();
+                        _encoder.estado();
+                        break;
+
+                    case "estado":
+                        _encoder.estado();
+                        break;
+
+                    case "salir":
+                        _encoder.salir();
+                        try
+                        {
+                            Thread.Sleep(3000);
+                        }
+                        catch (Exception e)
+                        {
+                        }
+                        noSalir = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Comando no reconocido");
+                        break;
+                }
+            }
+        }
+
+        public Program()
+        {
+
+        }
+    }
+}
